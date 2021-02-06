dc28949468edd52c7ca303a503681589f000ba9f
James Dingle <jdingle7@g.ucla.edu>
2021-02-05 22:18:36 -0800

    pogchamp
    yeee

---
+++ connect4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  5 22:11:12 2021
+
+@author: jamesdingle
+"""
+
+class Puck:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
