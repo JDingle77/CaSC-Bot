f0814ea7ef87d2f0bc5111524a413244e35421df
Sammy Shang <sammy7shang@gmail.com>
2021-02-06 15:54:13 -0800

    pygame implementation
    images

--- connect4.py
+++ connect4.py
@@ -1,59 +1,40 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb  5 22:11:12 2021
+Created on Fri Feb  5 23:28:44 2021
 
-@author: jamesdingle
+@author: sammy
 """
 
+import pygame
 import numpy as np
+
+
+pygame.init()
+#create screen
+screen = pygame.display.set_mode((1134,972))
+#background
+background = pygame.image.load("Connect4Board.png")
+#title and icon
+pygame.display.set_caption("Connect 4")
+
+#Player 1
+playerImg1 = pygame.image.load("PuckSmiley.png")
+
+#Player 2
+playerImg2 = pygame.image.load("PuckHeart.png")
+
+
+
+def save_file():
+    s=pygame.display.get_surface()
+    pygame.image.save(s, "connect4.png")
+    return
+
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
-        
-def create_board():
-    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
-    return board
 
-def drop_piece(board, row, col, piece):
-    board[row][col] = piece
 
-def is_valid_location(board, col):
-    return board[ROW_COUNT-1][col] == 0
-
-def get_next_open_row(board, col):
-    for r in range(ROW_COUNT):
-        if board[r][col] == 0:
-            return r
-
-def print_board(board):
-    print(np.flip(board, 0))
-    
-def winning_move(board, piece):
-    # Check horizontal locations for win
-    for c in range(COLUMN_COUNT - 3):
-        for r in range(ROW_COUNT):
-            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
-                return True
-            
-    # Check vertical locations for win
-    for c in range(COLUMN_COUNT):
-        for r in range(ROW_COUNT - 3):
-            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
-                return True
-            
-    # Check positively sloped diagonals
-    for c in range(COLUMN_COUNT - 3):
-        for r in range(ROW_COUNT - 3):
-            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
-                return True
-    
-    # Check negatively sloped diagonals
-    for c in range(COLUMN_COUNT - 3):
-        for r in range(3, ROW_COUNT):
-            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
-                return True
-            
 def return_coords():
     tuple1 = (13.5, 823.5)
     tuple2 = (175.5, 823.5)
@@ -108,13 +89,81 @@ def return_coords():
     tupleboard = (firsttuple, secondtuple, thirdtuple, fourthtuple, fifthtuple, sixthtuple)
     
     return tupleboard
+
+        
+def create_board():
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
+    return board
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+def is_valid_location(board, col):
+    return board[ROW_COUNT-1][col] == 0
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+def print_board(board):
+    print(np.flip(board, 0))
     
+def print_scene(board, coord):
+    screen.fill((0, 0, 0))
+    screen.blit(background, (0,0))
+    
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            if(board[r][c]==1):
+                screen.blit(playerImg1,(coord[r][c]))
+            if(board[r][c]==2):
+                screen.blit(playerImg2,(coord[r][c]))
+
+    pygame.display.update()
+    save_file()
+            
+def winning_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+            
+    # Check vertical locations for win
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+            
+    # Check positively sloped diagonals
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+    
+    # Check negatively sloped diagonals
+    for c in range(COLUMN_COUNT - 3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+            
+
 board = create_board()
-print_board(board)
+
 game_over = False
 turn = 0;
+coords = return_coords()
 
 while not game_over:
+    
+   
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            save_file()
+            game_over = True
+    print_scene(board, coords)
+    pygame.event.wait()
     # Ask for Player 1 Input
     if turn == 0:
         col = int(input("Player 1 Make your Selection (0-6): "))
@@ -154,8 +203,10 @@ while not game_over:
                 game_over = True
                 break
     
-    print_board(board)
     turn += 1
     turn = turn % 2
     
+pygame.display.quit()
+pygame.quit()
+
     
