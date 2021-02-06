8494c7d62a9c337a1a986832c4149ab2f9dbdcdb
NathanKY <78581743+NathanKY@users.noreply.github.com>
2021-02-06 09:48:55 -1000

    voice1.0
    joins a specified vc and leaves when told to.

--- botmain.py
+++ botmain.py
@@ -2,9 +2,10 @@
 """
 Created on Fri Feb  5 21:08:50 2021
 
-@author: sammy
+@author: nathan
 """
-
+import asyncio
+import discord
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '!')
@@ -14,7 +15,15 @@ async def on_ready():
     print('Bot is ready.')
     
 @client.command()
-async def ping(ctx):
-    await ctx.send(f'Ping is {round(client.latency * 1000)}ms')
+async def joinvc(ctx, *, channel: discord.VoiceChannel):
+    if ctx.voice_client is not None:
+        return await ctx.voice_client.move_to(channel)
+    await ctx.send('Hiii!')
+    await channel.connect()
+
+@client.command()
+async def leave(ctx):
+    await ctx.send('See you later!')
+    await ctx.voice_client.disconnect()
 
 client.run('ODA3NDcyNTI2MjE3MDUyMTYw.YB4fZA.qof50jXFk1kKTyOnlEIaIIAJ8MI')
