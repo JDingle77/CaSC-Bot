7110f1707e5b953e54eb88f9f81ca28dee5e6286
Sammy Shang <sammy7shang@gmail.com>
2021-02-05 21:29:31 -0800

    Added a token
    token

--- botmain.py
+++ botmain.py
@@ -8,3 +8,5 @@ Created on Fri Feb  5 21:08:50 2021
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '!')
+
+client.run('ODA3NDcyNTI2MjE3MDUyMTYw.YB4fZA.qof50jXFk1kKTyOnlEIaIIAJ8MI')
